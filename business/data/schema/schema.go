cbe68a1d3773de9b6b1efc6fa0fc2d223d2cf93e
didiyudha <didiyudha@gmail.com>
2021-04-01 20:26:30 +0700

    add delete all data characters functionality

--- business/data/schema/schema.go
+++ business/data/schema/schema.go
@@ -30,3 +30,20 @@ func Migrate(ctx context.Context, db *sqlx.DB) error {
 	d := darwin.New(driver, darwin.ParseMigrations(schemaDoc))
 	return d.Migrate()
 }
+
+func DeleteAll(ctx context.Context, db *sqlx.DB) error {
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	if _, err := tx.ExecContext(ctx, deleteDoc); err != nil {
+		if err := tx.Rollback(); err != nil {
+			return err
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
